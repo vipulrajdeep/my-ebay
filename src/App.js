4a51873e01a427dcc6dcbac955a4558e4cb6b63f
Rajdeep Vijayakar <rajdeepvijayakar@rajdeeps-air.mynet>
2020-03-30 01:53:21 +0100

    cleanup of console logs

--- src/App.js
+++ src/App.js
@@ -29,9 +29,6 @@ class App extends Component {
                 id: snapShot.id,
                 ...snapShot.data()
               }
-            },
-            () => {
-              console.log(this.state);
             }
           );
         });
