fee9602172d97b071e40915c0a0b18b221f2ed2d
Rajdeep Vijayakar <rajdeepvijayakar@192.168.1.103>
2020-03-30 01:39:04 +0100

    finished sign in

--- src/components/signin/sign-in.component.jsx
+++ src/components/signin/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import FormInput from "./../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "./../../firebase/firebase.utils";
+import { signInWithGoogle, auth } from "./../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -23,9 +23,18 @@ class SignInComponent extends Component {
     });
   };
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    console.log(this.state);
+    const {email, password} = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: "",
+        password: ""
+      })
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   render() {
